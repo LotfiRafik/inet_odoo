f51dc024fbc8a2d50ccb0f931b67225a34c0e705
Bouchafa Lotfi Rafik <gl_bouchafa@esi.dz>
2021-02-25 12:13:14 +0100

    Set algeria as default country when creating a partner record

--- odoo-custom-addons/inet_odoo_addons_base/models/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class PartnerCategory(models.Model):
-    _inherit = 'res.partner.category'
-
-    #Changing original String of the field "name" from 'Tag Name' To 'Category Name'
-    name = fields.Char(string='Category Name', required=True, translate=True)
---
+++ odoo-custom-addons/inet_odoo_addons_base/models/res_partner.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+    
+    @api.model
+    def default_get(self, default_fields):
+        """Set Algeria as default country of a partner"""
+        values = super().default_get(default_fields)
+        values['country_id'] = 62
+        return values
+
+
+class PartnerCategory(models.Model):
+    _inherit = 'res.partner.category'
+
+    #Changing original String of the field "name" from 'Tag Name' To 'Category Name'
+    name = fields.Char(string='Category Name', required=True, translate=True)
